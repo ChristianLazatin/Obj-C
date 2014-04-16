fe2aebf84ed6bfebca0de337d90681501fa7bf9e
ChristianLazatin <c.lazatin@klab.com>
2014-04-16 13:55:38 +0800

    Challenge in Chapter 6

---
+++ Chapter6/Math/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Math
+//
+//  Created by Christian Ramir Lazain on 4/16/14.
+//  Copyright (c) 2014 klab. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[])
+{
+    //Display the sine of 1 in radians
+    float sin_one = sin(1);
+    printf("sine of 1 is %.3f", sin_one);
+    return 0;
+}
+
