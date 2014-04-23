f172e6e362194df661cbe2d1bfd209dd93de2539
ChristianLazatin <c.lazatin@klab.com>
2014-04-23 14:19:54 +0800

    Chapter8, pointers

---
+++ Chapter8/pointer/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  pointer
+//
+//  Created by Christian Ramir Lazain on 4/23/14.
+//  Copyright (c) 2014 klab. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    int i = 17;
+    int *addressOfI = &i;
+    printf("i stores its value at %p\n", &i);
+    printf("the int stored at addressOfI is %d\n", *addressOfI);
+    *addressOfI = 89;
+    printf("Now i is %d\n", i);
+    printf("An int is %zu bytes\n", sizeof(int));
+    printf("A pointer is %zu bytes\n", sizeof(addressOfI));
+    
+    return 0;
+}
+
---
+++ Chapter8/sizeOfFloat/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  sizeOfFloat
+//
+//  Created by Christian Ramir Lazain on 4/16/14.
+//  Copyright (c) 2014 klab. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    
+    printf("A float consumes %d bytes of memory\n", sizeof(float));
+    
+    return 0;
+}
+
